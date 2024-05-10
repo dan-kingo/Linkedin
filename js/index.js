557211ccc9cec23bdc4cd4fb1c45e52a1d885080
Dan Kingo <danieldejen23@gmail.com>
2024-05-10 16:11:42 +0300

    finish setting display container

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let profile = document.getElementById("profile");
+
+let profilePic = document.getElementById("profile-pic");
+profilePic.onclick = function () {
+  profile.classList.toggle("active-profile");
+};
